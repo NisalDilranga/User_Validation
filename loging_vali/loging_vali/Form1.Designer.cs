320f9b48585343db6420a7084649842a14327ced
Nisal Dilranga <128956345+Nawron@users.noreply.github.com>
2024-01-15 11:01:41 +0530

    form.cs
    Full code file

---
+++ loging_vali/loging_vali/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace loging_vali
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.lbl_Name = new System.Windows.Forms.Label();
+            this.lbl_pass = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.Location = new System.Drawing.Point(169, 316);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(75, 23);
+            this.btn_Login.TabIndex = 0;
+            this.btn_Login.Text = "Login";
+            this.btn_Login.UseVisualStyleBackColor = true;
+            this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click);
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.Location = new System.Drawing.Point(365, 316);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(75, 23);
+            this.btn_Close.TabIndex = 1;
+            this.btn_Close.Text = "Close";
+            this.btn_Close.UseVisualStyleBackColor = true;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // lbl_Name
+            // 
+            this.lbl_Name.AutoSize = true;
+            this.lbl_Name.Location = new System.Drawing.Point(75, 111);
+            this.lbl_Name.Name = "lbl_Name";
+            this.lbl_Name.Size = new System.Drawing.Size(76, 16);
+            this.lbl_Name.TabIndex = 2;
+            this.lbl_Name.Text = "User Name";
+            // 
+            // lbl_pass
+            // 
+            this.lbl_pass.AutoSize = true;
+            this.lbl_pass.Location = new System.Drawing.Point(75, 195);
+            this.lbl_pass.Name = "lbl_pass";
+            this.lbl_pass.Size = new System.Drawing.Size(67, 16);
+            this.lbl_pass.TabIndex = 3;
+            this.lbl_pass.Text = "Password";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(228, 95);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(165, 22);
+            this.txtUser.TabIndex = 4;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(228, 189);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(165, 22);
+            this.txtPass.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 501);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.lbl_pass);
+            this.Controls.Add(this.lbl_Name);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.btn_Login);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Login;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.Label lbl_Name;
+        private System.Windows.Forms.Label lbl_pass;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPass;
+    }
+}
+
